008fd411ca890cd342cd6fe19db152d73ca69ef0
Gavrieee <167282522+Gavrieee@users.noreply.github.com>
2024-09-01 19:57:50 +0800

    Add files via upload

---
+++ schema.sql.sql
@@ -0,0 +1,42 @@
+create table Users (
+	UserID INT,
+	Username VARCHAR(50),
+	FirstName VARCHAR(50),
+	LastName VARCHAR(50),
+	DateOfBirth DATE,
+	Password VARCHAR(50),
+	DateAdded DATE
+);
+
+create table Friends (
+	FriendID INT,
+	FriendWhoAdded INT,
+	FriendBeingAdded INT,
+	IsAccepted VARCHAR(50),
+	DateAdded DATE
+);
+
+create table Groups (
+	GroupID INT,
+	GroupName VARCHAR(50),
+	CreatedBy INT,
+	DateAdded DATE
+);
+
+create table Posts (
+	PostID INT,
+	PostDescription TEXT,
+	PostedBy INT,
+	IsPublic VARCHAR(50),
+	IsOnlyForFriends VARCHAR(50),
+	GroupID INT,
+	DatePosted DATE
+);
+
+create table GroupMembershipRequests (
+	GroupMemberShipRequestsID INT,
+	GroupID INT,
+	GroupMemberUserID INT,
+	IsGroupMemberShipAccepted VARCHAR(50),
+	DateAccepted DATE
+);
